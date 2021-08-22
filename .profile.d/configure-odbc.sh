c07e22881a08cfd880dea4092e54dfaf7364c92e
mshanmugam80 <shanmca2009@gmail.com>
2021-08-22 20:20:27 +0530

    Update configure-odbc.sh

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -4,13 +4,6 @@ export ODBCSYSINI=${HOME}/.apt/usr/lib/odbc/conf/
 
 echo "[ODBC Driver 17 for SQL Server]
 Description=Microsoft ODBC Driver 17 for SQL Server
-Driver=/app/.apt/usr/lib/libmsodbcsql-17.5.so.2.1
+Driver=/app/.apt/usr/lib/libmsodbcsql-17.6.so.1.1
 UsageCount=1
-
-PORT=1433
-CLIENT_SESSION_KEEP_ALIVE=true
-uid=${SQLUSERNAME}
-pwd=${SQLUSERPASSWORD}
-server=${SQLSERVER}
-database=${SQLDATABASE}
 " > ${ODBCSYSINI}/odbc.ini
